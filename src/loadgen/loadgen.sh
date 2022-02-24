0e2ca71a28c95fa0073b50a0ff05d1c95844aac5
Brad Schmitt <bschmitt@newrelic.com>
2022-02-24 11:51:47 -0600

    update loadgen

--- src/loadgen/loadgen.sh
+++ src/loadgen/loadgen.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+PORT=$1
+HOST=frontend
+
 while true; do
     for i in {1..1000}
     do
-        HOST=frontend
         MESSAGE=$(fortune | tr -d '\n')
         echo "Load generator: posting to $HOST message: $MESSAGE"
-        curl --data-urlencode "message=$MESSAGE" -X POST http://$HOST/message --max-time 10
+        curl --data-urlencode "message=$MESSAGE" -X POST http://$HOST:$PORT/message --max-time 10
         sleep $(( ( RANDOM % 10 )  + 1 ))
     done
 done
